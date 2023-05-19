b97b9c9d744856d8f1bf5ed3f58d91ec805ed067
HiSerg71 <108295720+HiSerg71@users.noreply.github.com>
2023-05-19 22:07:59 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeColor() {
+let h1 = document.getélenentById('text');
+h1.style.color = 'green';
+}
+function hightignt() {
+let h1 = document.getElementById('text');
+h1.classList.add('nightignt');
+}
